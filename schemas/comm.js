cca38171414d1823b00875a36ff59e078a005657
Harman <singh9765h@gmail.com>
2023-03-03 20:13:58 +0530

    Backend from sanity

---
+++ schemas/comm.js
@@ -0,0 +1,35 @@
+export default {
+    name: "comms",
+    title: "Comms",
+    type: "document",
+    fields: [
+      {
+        name: "title",
+        title: "Title",
+        type: "string",
+      },
+      {
+        name: "description",
+        title: "Description",
+        type: "string",
+      },
+      {
+        name: "imgUrl",
+        title: "ImgUrl",
+        type: "image",
+        options: {
+          hotspot: true,
+        },
+      },
+      {
+        name: "link",
+        title: "Link",
+        type: "string",
+      },
+      {
+        name: "twitterlink",
+        title: "Twitter Link",
+        type: "string",
+      },
+    ],
+  };
--- schemas/index.js
+++ schemas/index.js
@@ -1,1 +1,12 @@
-export const schemaTypes = []
+
+import abouts from './abouts'
+import testimonial from './testimonial'
+import brands from './brands'
+import comm from './comm'
+import contact from './contact'
+import experiences from './experiences'
+import skills from './skills'
+import workExperience from './workExperience'
+import works from './works'
+
+export const schemaTypes =[testimonial,abouts,brands,comm,contact,experiences,skills,workExperience,works]
